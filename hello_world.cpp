fb0f53d3d576c8b8b3c1ff35a61462f79b13cca4
Ubuntu <ubuntu@ip-172-31-24-146.ec2.internal>
2019-09-16 23:40:38 +0000

    send file to git hub

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 St. Mother Teresa HS All rights reserved
+//
+// Created by: Mariam Hemdan
+// Created on: September 2019
+// This is the "Hello, World!" program, but with nice style.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello ,World!" << std::endl;
+}
